c8123d7b6cc50aacd08d58ae5efa9c2e4e9c931b
boobooly <ben_dy_@163.com>
2015-11-19 16:30:25 +0800

    dllmain

--- main/Beyond.APM.Profiler/dllmain.h
+++ main/Beyond.APM.Profiler/dllmain.h
@@ -5,6 +5,7 @@ class CBeyondAPMProfilerModule : public ATL::CAtlDllModuleT< CBeyondAPMProfilerM
 public :
 	DECLARE_LIBID(LIBID_BeyondAPMProfilerLib)
 	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_BEYONDAPMPROFILER, "{C2D01FD4-74BF-4B5D-985B-109DCDA2AE9B}")
+	HINSTANCE m_hModule;
 };
 
 extern class CBeyondAPMProfilerModule _AtlModule;
